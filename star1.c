058e8786bd58c54ce25153a5768f3c38a519a8c9
roshinisk <roshini.skumar15@gmail.com>
2020-08-07 13:00:01 +0530

    Update star1.c

--- star1.c
+++ star1.c
@@ -4,26 +4,26 @@ int main(){
   printf("Enter the number of rows\n");  
   scanf("%d",&n);  
   int spaces=n-1;  
-  int stars=1;  
+  int star=1;  
   for(int i=1;i<=n;i++)  
   {  
     for(int j=1;j<=spaces;j++)  
     {  
       printf(" ");  
     }  
-    for(int k=1;k<=stars;k++)  
+    for(int k=1;k<=star;k++)  
     {  
       printf("*");  
     }  
     if(spaces>i)  
     {  
       spaces=spaces-1;  
-      stars=stars+2;  
+      star=star+2;  
     }  
     if(spaces<i)  
     {  
       spaces=spaces+1;  
-      stars=stars-2;  
+      star=star-2;  
     }  
     printf("\n");  
   }  
